b1780f4fd766578b2eb9553a6c127a1f8f368f5d
rasim99 <rasimjm@code.edu.az>
2023-06-04 01:23:58 +0400

    add basket-part1

--- FrontToBack/Controllers/BasketController.cs
+++ FrontToBack/Controllers/BasketController.cs
@@ -1,24 +1,64 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FrontToBack.DAL;
+using FrontToBack.Models;
+using FrontToBack.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace FrontToBack.Controllers
 {
     public class BasketController : Controller
     {
+        private readonly AppDbContext _appDbContext;
+
+        public BasketController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-         public IActionResult AddBasket()
+         public IActionResult AddBasket(int ?id)
         {
-            //HttpContext.Session.SetString("p515", "programming group");
-            Response.Cookies.Append("p515", "programming group", new CookieOptions { MaxAge = TimeSpan.FromMinutes(15) });
-            return Content("set olundu");
+            if (id == null) return NotFound();
+            var product = _appDbContext.Products.Find(id);
+            if (product == null) return NotFound();
+            var basket = Request.Cookies["basket"];
+            List<BasketVM> products;
+            if(basket == null)
+            {
+                 products= new List<BasketVM>();
+            }
+            else
+            {
+                products = JsonConvert.DeserializeObject<List<BasketVM>>(basket);
+            }
+            var existProduct = products.Find(x => x.Id == product.Id);
+            if (existProduct == null)
+            {
+                BasketVM basketVM = new BasketVM()
+                {
+                    Id = product.Id,
+                    Name = product.Name,
+                    BasketCount=1
+                };
+                products.Add(basketVM);
+
+            }
+            else
+            {
+                existProduct.BasketCount++;
+            }
+         
+            Response.Cookies.Append("basket", JsonConvert.SerializeObject(products), new CookieOptions { MaxAge = TimeSpan.FromMinutes(10) });
+            return Content($" {product.Name} set olundu");
         }
         public IActionResult ShowBasket()
         {
-            //string result= HttpContext.Session.GetString("p515");
-            string result = Request.Cookies["p515"];
-            return Content(result);
+            var basket = Request.Cookies["basket"];
+            var result=JsonConvert.DeserializeObject<List<BasketVM>>(basket);
+            return Json(result);
         }
     }
 }
---
+++ FrontToBack/ViewModels/BasketVM.cs
@@ -0,0 +1,15 @@
+﻿using FrontToBack.Models;
+
+namespace FrontToBack.ViewModels
+{
+    public class BasketVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+        public int BasketCount { get; set; }
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
