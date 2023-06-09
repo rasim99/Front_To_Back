df0e8160df70d43c4b29e747c6ce24b8f1d89be5
rasim99 <rasimjm@code.edu.az>
2023-06-10 00:51:29 +0400

    add detail action & create action

--- FrontToBack/Areas/Adminarea/Controllers/CategoryController.cs
+++ FrontToBack/Areas/Adminarea/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using FrontToBack.DAL;
+using FrontToBack.Models;
+using FrontToBack.ViewModels.AdminVM.Category;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace FrontToBack.Areas.Adminarea.Controllers
 {
@@ -16,6 +19,30 @@ namespace FrontToBack.Areas.Adminarea.Controllers
         public IActionResult Index()
         {
             return View(_appDbContext.Categories.ToList());
+        }
+        
+        public IActionResult Detail(int ?id)
+        {
+            if (id == null) return NotFound();
+            var category = _appDbContext.Categories.FirstOrDefault(i => i.Id == id);
+            if (category == null) return NotFound();
+            return View(category);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Create( CategoryVM categoryVM )
+        {
+            if (!ModelState.IsValid) return View();
+            Category newcategory = new();
+            newcategory.Name = categoryVM.Name;
+            newcategory.Desc = categoryVM.Desc;
+            _appDbContext.Categories.Add(newcategory);
+            _appDbContext.SaveChanges();
+            return Content(categoryVM.Name + "  " + categoryVM.Desc);
         }
     }
 }
---
+++ FrontToBack/ViewModels/AdminVM/Category/CategoryVM.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrontToBack.ViewModels.AdminVM.Category
+{
+    public class CategoryVM
+    {
+        [Required]
+        [MaxLength(10)]
+        public string Name { get; set; }
+        [Required]
+        [MinLength(50, ErrorMessage = "50-den az ola bilmez")]
+        public string Desc { get; set; }
+    }
+}
